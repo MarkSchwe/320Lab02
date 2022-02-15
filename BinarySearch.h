095662518250c9e64a8f8a383cd65474fbd25777
MarkSchwe <91101294+MarkSchwe@users.noreply.github.com>
2022-02-15 14:28:19 -0500

    Update BinarySearch.h

--- BinarySearch.h
+++ BinarySearch.h
@@ -4,54 +4,56 @@ using namespace std;
 
 class BinarySearch{
 public:
-    int Bsearch(int arr[],int start, int end, int search);
+    int Bsearch(int arr[],int start, int end, int search, int &fails);
 
 };
 
-int BinarySearch::Bsearch(int arr[], int start, int end, int search){
+int BinarySearch::Bsearch(int arr[], int start, int end, int search, int &fails){
     //cout << search << endl;
 //cout << arr[start] << " " << arr[end] << endl;
 int middle = (end-1 + start)/2;
 //cout << arr[middle] << endl;
 if((start == end+1) || (start+1 == end)){
     if(arr[start] == search){
-        cout << "Item found" << endl;
+        //cout << "Item found" << endl;
         return arr[start];
     }
     if(arr[end] == search){
-        cout << "Item found" << endl;
+        //cout << "Item found" << endl;
         return arr[end];
     }
     if(arr[start+1] == search){
-        cout << "Item found" << endl;
+        //cout << "Item found" << endl;
         return arr[start];
     }
     if(arr[end-1] == search){
-        cout << "Item found" << endl;
+        //cout << "Item found" << endl;
         return arr[end];
     }
     if(arr[end] != search && arr[start] != search){
-        cout << "Item not found" << endl;
+        //cout << "Item not found" << endl;
         return arr[middle];
     }
 }
 if((arr[middle] == search) || (arr[start] == arr[end]) || arr[end] == arr[middle] || arr[start] == arr[middle]){
     if(arr[middle] == search){
-        cout << "Item is found" << endl;
+        //cout << "Item is found" << endl;
         return arr[middle];
     }
     if(start == end || arr[middle] != search){
-        cout << "item not found" << endl;
+        //cout << "item not found" << endl;
         return arr[end];
     }
 }
 if(search < arr[middle] && start != end){
 //cout << "TestSmall" << endl;
-return Bsearch(arr,start,middle,search);
+fails++;
+return Bsearch(arr,start,middle,search, fails);
 }
 //cout << "TestBig" << endl;
 if(search > arr[middle] && start != end){
-return Bsearch(arr,middle,end,search);
+fails++;
+return Bsearch(arr,middle,end,search, fails);
 }
 return 0; 
 }
